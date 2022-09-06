cb7effa6cbed2631b0414456787240bd9a4ba9f9
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-09-06 10:50:27 +0200

    User Domain Models

---
+++ user.core/src/main/java/com/springbank/user/core/models/Account.java
@@ -0,0 +1,17 @@
+package com.springbank.user.core.models;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Account {
+    private String username;
+    private String password;
+    private List<Role> roles;
+}
