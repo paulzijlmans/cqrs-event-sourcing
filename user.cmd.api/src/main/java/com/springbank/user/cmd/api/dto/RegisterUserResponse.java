cb7cd2e8145a451d2b570b1f1a59bb8b81ae14a6
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-10-06 11:20:09 +0200

    REST Controller for Registering Users

---
+++ user.cmd.api/src/main/java/com/springbank/user/cmd/api/dto/RegisterUserResponse.java
@@ -0,0 +1,8 @@
+package com.springbank.user.cmd.api.dto;
+
+public class RegisterUserResponse extends BaseResponse {
+
+    public RegisterUserResponse(String message) {
+        super(message);
+    }
+}
--- user.core/src/main/java/com/springbank/user/core/models/Account.java
+++ user.core/src/main/java/com/springbank/user/core/models/Account.java
@@ -1,17 +1,24 @@
 package com.springbank.user.core.models;
 
 import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Account {
+    @Size(min = 2, message = "username must have a minimum of two characters")
     private String username;
+    @Size(min = 2, message = "username must have a minimum of seven characters")
     private String password;
+    @NotNull(message = "specify at least one role")
     private List<Role> roles;
 }
