d9b8ac6c4188fef5cc012cd0ffa282009eab30ea
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-10-06 16:26:16 +0200

    User Query Handler

---
+++ user.query.api/src/main/java/com/springbank/user/query/api/dto/UserLookupResponse.java
@@ -0,0 +1,19 @@
+package com.springbank.user.query.api.dto;
+
+import com.springbank.user.core.models.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class UserLookupResponse {
+    private List<User> users;
+
+    public UserLookupResponse(User user) {
+        this.users = new ArrayList<>();
+        this.users.add(user);
+    }
+}
