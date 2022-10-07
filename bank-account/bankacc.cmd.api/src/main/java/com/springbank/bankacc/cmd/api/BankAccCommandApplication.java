db55f70b93b08da65830c68159c0d2c29854b5a4
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-10-07 15:11:14 +0200

    Bank Account Project Setup

---
+++ bank-account/bankacc.cmd.api/src/main/java/com/springbank/bankacc/cmd/api/BankAccCommandApplication.java
@@ -0,0 +1,21 @@
+package com.springbank.bankacc.cmd.api;
+
+import com.springbank.bankacc.core.configuration.AxonConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Import({AxonConfig.class})
+public class BankAccCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccCommandApplication.class, args);
+	}
+
+}
