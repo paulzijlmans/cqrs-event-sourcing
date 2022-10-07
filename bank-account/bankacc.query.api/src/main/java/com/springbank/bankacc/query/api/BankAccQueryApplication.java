75d85f9d30e078193e41b75a5625f95b2f39a943
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-10-07 15:20:23 +0200

    Bank Account Domain Models

--- bank-account/bankacc.query.api/src/main/java/com/springbank/bankacc/query/api/BankAccQueryApplication.java
+++ bank-account/bankacc.query.api/src/main/java/com/springbank/bankacc/query/api/BankAccQueryApplication.java
@@ -3,13 +3,15 @@ package com.springbank.bankacc.query.api;
 import com.springbank.bankacc.core.configuration.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@SpringBootApplication
 @EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@SpringBootApplication
+@EntityScan(basePackages = "com.springbank.bankacc.core.models")
 @Import({AxonConfig.class})
 public class BankAccQueryApplication {
 
