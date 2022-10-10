5a61f719fc5c7927a1d757a5a076a950fd03a7e5
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-10-10 11:48:13 +0200

    REST Controller for Opening a Bank Account

--- bank-account/bankacc.core/src/main/java/com/springbank/bankacc/core/events/AccountOpenedEvent.java
+++ bank-account/bankacc.core/src/main/java/com/springbank/bankacc/core/events/AccountOpenedEvent.java
@@ -1,13 +1,17 @@
 package com.springbank.bankacc.core.events;
 
 import com.springbank.bankacc.core.models.AccountType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AccountOpenedEvent {
     private String id;
     private String accountHolderId;
--- bank-account/bankacc.core/src/main/java/com/springbank/bankacc/core/events/FundsDepositedEvent.java
+++ bank-account/bankacc.core/src/main/java/com/springbank/bankacc/core/events/FundsDepositedEvent.java
@@ -1,10 +1,14 @@
 package com.springbank.bankacc.core.events;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FundsDepositedEvent {
     private String id;
     private double amount;
