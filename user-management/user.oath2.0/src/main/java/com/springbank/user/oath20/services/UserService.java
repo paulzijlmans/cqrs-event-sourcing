56c08c762c587d3608c67eee51cb70243ee733ac
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-10-11 22:39:23 +0200

    Add default user

--- user-management/user.oath2.0/src/main/java/com/springbank/user/oath20/services/UserService.java
+++ user-management/user.oath2.0/src/main/java/com/springbank/user/oath20/services/UserService.java
@@ -2,6 +2,7 @@ package com.springbank.user.oath20.services;
 
 import com.springbank.user.oath20.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,6 +12,15 @@ import org.springframework.stereotype.Service;
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
 
+    @Value("${spring.security.user.name}")
+    private String adminUserName;
+
+    @Value("${spring.security.user.password}")
+    private String adminPassword;
+
+    @Value("${spring.security.user.roles}")
+    private String adminRoles;
+
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -18,6 +28,18 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (username.equals(adminUserName)) {
+            return org.springframework.security.core.userdetails.User
+                    .withUsername(adminUserName)
+                    .password(adminPassword)
+                    .authorities(adminRoles)
+                    .accountExpired(false)
+                    .accountLocked(false)
+                    .credentialsExpired(false)
+                    .disabled(false)
+                    .build();
+        }
+
         var user = userRepository.findByUsername(username);
 
         if (user.isEmpty()) {
